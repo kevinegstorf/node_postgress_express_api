1b59dbd8d0d382d3f488503efe78fdfd072492de
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-04 13:21:09 +0200

    added get requests for index and id

--- db/index.js
+++ db/index.js
@@ -3,8 +3,5 @@ const { user, host, database, password, port } = require('../secrets/db_config')
 
 const pool = new Pool({ user, host, database, password, port });
 
-pool.query('SELECT * FROM monsters', (err, res)=> {
-  if(err) return console.log('error!!', err);
-  
-  console.log(res);
-});
+
+module.exports = pool;
